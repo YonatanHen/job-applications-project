d41c8a197c0708159744deb817e69c793d6e67c1
YehonatanHen <yonatan.h333@gmail.com>
2022-09-30 11:49:38 +0300

    create update & delete application funcs

--- src/users/services/applications/applications.service.ts
+++ src/users/services/applications/applications.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Application } from 'src/entitites/Application';
 import { User } from 'src/entitites/User';
 import { ApplicationParams } from 'src/utils/types';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 
 @Injectable()
 export class ApplicationsService {
@@ -18,7 +18,6 @@ export class ApplicationsService {
     // get user applications
     async userApplications(username: string): Promise<Application[]> {
         const user = await this.userRepository.findOneBy({ username })
-        console.log(user)
         return this.applicationRepository.findBy({ user: user })
     }
 
@@ -32,6 +31,22 @@ export class ApplicationsService {
     }
 
     // update application
+    async updateApplication(username: string, applicationId: number, applicationDetails: ApplicationParams): Promise<UpdateResult> {
+        const application = await this.applicationRepository.findOneBy({ id: applicationId })
+        
+        // Check if the application belongs to the user who sent the request
+        if(application.user.username === username)
+            return await this.applicationRepository.update({ id: applicationId }, { ...applicationDetails })
+        else throw new HttpException("You are not allowed to update this application",HttpStatus.BAD_REQUEST)
+    }
 
     // delete application
+    async deleteApplication(username: string, applicationId: number, applicationDetails: ApplicationParams): Promise<DeleteResult> {
+        const application = await this.applicationRepository.findOneBy({ id: applicationId })
+        
+        // Check if the application belongs to the user who sent the request
+        if(application.user.username === username)
+            return await this.applicationRepository.delete({ id: applicationId })
+        else throw new HttpException("You are not allowed to delete this application",HttpStatus.BAD_REQUEST)
+    }
 }
